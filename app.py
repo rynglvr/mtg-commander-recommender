487f41d708641f4baac5f117cbf8b966f60c4346
Ryan Glover <ryn.glvr@gmail.com>
2025-06-08 22:44:11 -0600

    Update app.py for deployment

--- app.py
+++ app.py
@@ -273,4 +273,5 @@ def recommend():
         return jsonify({"error": f"Server error: {str(e)}"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    # app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
